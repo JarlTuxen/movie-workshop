7dcef53f09efe29ce6c3f0fffe826180f23489a7
jarltuxen <jarl.tuxen@gmail.com>
2022-02-08 18:37:18 +0100

    lambda expression

--- src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
+++ src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
@@ -12,14 +12,29 @@ public class MovieController {
     //Controller methods
     @GetMapping("/")
     public String index(){
-        return "Hello World welcome to my application";
+        return "Hello World welcome to my application" +
+                "/getFirst" +
+                "/getRandom" +
+                "/getTenSortByPopularity" +
+                "/howManyWonAnAward" +
+                "(Advanced) /filter?char=’x’amount=’n’" +
+                "(Advanced) /longest?g1=’x’g2=’y’";
     }
 
     @GetMapping("/findall")
     public String findAll(){
         MovieDataRepository movieDataRepository = new MovieDataRepository();
         List<Movie> movieList = movieDataRepository.findAll();
-        return "all movies:" + movieList;
+        String movies = "<h1>All movies</h1>";
+        for (Movie m:movieList){
+            movies += m.toString() + "<br>";
+        }
+        /* lambda expression
+        String movies = movieList
+                .stream()
+                .reduce("", (substring, element) -> substring + element.toString() + "<br>", String::concat);
+        */
+        return movies;
 
     }
 }
