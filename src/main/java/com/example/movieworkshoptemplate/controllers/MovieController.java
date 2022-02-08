6ef5cd1437a68828c916acb893814b25bf3faad2
jarltuxen <jarl.tuxen@gmail.com>
2022-02-08 18:16:58 +0100

    imdb-data.csv moved and marked as resource root-folder read all created

--- src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
+++ src/main/java/com/example/movieworkshoptemplate/controllers/MovieController.java
@@ -1,7 +1,11 @@
 package com.example.movieworkshoptemplate.controllers;
 
+import com.example.movieworkshoptemplate.models.Movie;
+import com.example.movieworkshoptemplate.repositories.MovieDataRepository;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class MovieController {
@@ -9,5 +13,13 @@ public class MovieController {
     @GetMapping("/")
     public String index(){
         return "Hello World welcome to my application";
+    }
+
+    @GetMapping("/findall")
+    public String findAll(){
+        MovieDataRepository movieDataRepository = new MovieDataRepository();
+        List<Movie> movieList = movieDataRepository.findAll();
+        return "all movies:" + movieList;
+
     }
 }
--- src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
+++ src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
@@ -1,9 +1,11 @@
 package com.example.movieworkshoptemplate.repositories;
 
 import com.example.movieworkshoptemplate.models.Movie;
+import org.springframework.core.io.ClassPathResource;
 
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.IOException;
 import java.util.LinkedList;
 import java.util.List;
 import java.util.Scanner;
@@ -16,14 +18,37 @@ public class MovieDataRepository {
 
         try
         {
-            Scanner input = new Scanner(new File("imdb-data.scv"));
+            File file = new ClassPathResource("imdb-data.csv").getFile();
+            Scanner input = new Scanner(file);
+            //Scanner input = new Scanner(new File("imdb-data.csv"));
+            //skip headers
+            System.out.println(input.nextLine());
             while (input.hasNextLine()){
-                System.out.println(input.nextLine());
+                //read line and create movie
+                //Year;Length;Title;Subject;Popularity;Awards
+
+                //use scanner to read line
+                Scanner line = new Scanner(input.nextLine()).useDelimiter(";");
+
+                Movie movie = new Movie();
+                movie.setYear(line.nextInt());
+                movie.setLength(line.nextInt());
+                movie.setTitle(line.next());
+                movie.setSubject(line.next());
+                movie.setPopularity(line.nextInt());
+                String award = line.next();
+                movie.setAwards(award.equals("Yes"));
+
+                movieList.add(movie);
             }
         }
         catch(FileNotFoundException e)
         {
             System.out.println("Error reading file: " + e);
+        }
+        catch (IOException e)
+        {
+            System.out.println("IO Exception: " + e);
         }
         return movieList;
     }
