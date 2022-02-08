00b35c29a8babde516a6727f0424b7862a79c660
jarltuxen <jarl.tuxen@gmail.com>
2022-02-08 16:32:40 +0100

    Filehandling

--- src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
+++ src/main/java/com/example/movieworkshoptemplate/repositories/MovieDataRepository.java
@@ -1,5 +1,30 @@
 package com.example.movieworkshoptemplate.repositories;
 
+import com.example.movieworkshoptemplate.models.Movie;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
 public class MovieDataRepository {
     //Methods to get data from file
+
+    public List<Movie> findAll() {
+        List<Movie> movieList = new LinkedList<>();
+
+        try
+        {
+            Scanner input = new Scanner(new File("imdb-data.scv"));
+            while (input.hasNextLine()){
+                System.out.println(input.nextLine());
+            }
+        }
+        catch(FileNotFoundException e)
+        {
+            System.out.println("Error reading file: " + e);
+        }
+        return movieList;
+    }
 }
